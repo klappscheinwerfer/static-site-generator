476a39bd53459c2a494f56df36c33c0544a303d4
klappscheinwerfer <toms.grants@inbox.lv>
2022-07-26 23:44:50 +0300

    Parse arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 
 int main(int argc, char *argv[]) {
 
     // Parse arguments
 
-    std::string inputPath = "";
-    std::string outputPath = "";
+    std::string inputPath, outputPath;
 
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << "\n";
-        /*if(std::string("-i").compare(argv[i])) {
-            std::cout << argv[i + 1];
-        }*/
+    for (int i = 1; i < argc - 1; i++) {
+        if(std::string("-i").compare(argv[i]) == 0) {
+            inputPath = argv[i + 1];
+        }
+        if(std::string("-o").compare(argv[i]) == 0) {
+            outputPath = argv[i + 1];
+        }
+    }
+
+    // Validate input and output paths
+
+    if (!std::filesystem::exists(inputPath)) {
+        std::cout << inputPath << ": No such file or directory\n";
+        return 1;
+    }
+    if (!std::filesystem::exists(outputPath)) {
+        std::cout << outputPath << ": No such file or directory\n";
+        return 2;
     }
 
     return 0;
